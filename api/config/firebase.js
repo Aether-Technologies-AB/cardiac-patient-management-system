c91130b881b59e098f4754c8d05b0a4673d6fef4
Giancarlol <gian_pc21@hotmail.com>
2026-01-14 15:10:56 +0100

    Fix Firebase private key decoding for Vercel

--- api/config/firebase.js
+++ api/config/firebase.js
@@ -4,7 +4,7 @@ const admin = require('firebase-admin');
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n').replace(/"/g, '')
 };
 
 if (!admin.apps.length) {
